91d6f763116dccf8b9450829bc0c2dc2929a37b0
sebascanogh <sebastianmariocano@gmail.com>
2023-03-11 13:05:53 -0300

    Add files via upload

---
+++ ClasePrincipalTP.java
@@ -0,0 +1,66 @@
+package PrimerPaquete;
+
+public class ClasePrincipalTP {
+
+	public static void main(String[] args) {
+		//Punto 1.a TP
+		/*int NumeroInicio=5;
+		int NumeroFin=14;
+		while (NumeroInicio<=NumeroFin) {
+			System.out.println( "." +NumeroInicio);
+			NumeroInicio=NumeroInicio+1;
+		}*/
+		//Punto 1.b
+		
+		/*int NumeroInicio=5;
+		int NumeroFin=14;
+		while (NumeroInicio<=NumeroFin){
+		if	(NumeroInicio%2 == 0){
+			System.out.print("-" +NumeroInicio);
+		}
+			NumeroInicio=NumeroInicio+1;
+			
+		}*/
+		
+		//Punto 1.c
+		/*int NumeroInicio=5;
+		int NumeroFin=14;
+		boolean Pares= true; //para determinar par/impar escoger true(par)-false(impar)//
+		while (NumeroInicio<=NumeroFin){
+			if (Pares==true){
+			if	(NumeroInicio%2==0){
+				System.out.print("." +NumeroInicio);
+				}//cierre del if
+			
+			}else {
+				if(NumeroInicio%2!=0) {
+					System.out.print("." +NumeroInicio);
+				}
+				
+			}
+			NumeroInicio=NumeroInicio+1;
+		}*/
+		
+		//Punto 1.d
+		/*int  NumeroInicio=5;
+		for(int NumeroFin=14; NumeroFin>=NumeroInicio;NumeroFin--) {
+			 {
+				
+			}
+			System.out.println(NumeroFin);
+		}*/
+		
+		
+		}
+
+		}
+		
+		
+		
+			
+
+		
+
+	
+
+
